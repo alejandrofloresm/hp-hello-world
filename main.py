4abdf777540c4da682ddca530b2e6f94a9752844
Alejandro Flores <alejandro@esporaestudio.com>
2019-08-01 15:18:39 -0500

    Ejemplo de tipos de datos

--- main.py
+++ main.py
@@ -2,3 +2,19 @@
 
 # Imprimir una cadena de texto
 print("Hello world")
+
+# Enteros
+i_am_int = 5
+print(i_am_int, "es un entero (int)")
+
+# Flotantes
+i_am_float = 5.5
+print(i_am_float, "es un flotantes (float)")
+
+# String
+i_am_string = "Yo soy cadenín"
+print("-",i_am_string,"- es una cadena (string)")
+
+# Boolean
+i_am_bool = True
+print(i_am_bool, "es un booleano (bool)")
