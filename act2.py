c2e6b5ed74de88c7f749c110cafddf9487cbb521
Alejandro Flores <alejandro@esporaestudio.com>
2019-08-01 19:15:18 -0500

    Se agregan las soluciones de la actividad 2

--- act2.py
+++ act2.py
@@ -1,16 +1,24 @@
 # Ejercicio 2
 # 1) Revisa si son palíndomos
+def reverse_str(str):
+  return str[::-1]
+
+def is_palindrome(str):
+  return str == reverse_str(str)
+
 print("Ejercicio 1: Palíndomos")
 print("========================")
 print("¿Qué palabra quieres revisar por palíndromos?")
-inp = "COLOCA AQUÍ TU CÓDIGO"
-res = True
+inp = input()
+res = is_palindrome(inp)
 print("La palabra -", inp, "- es un palíndomo:", res)
 
 print("\n\n\nEjercicio 2: Non o par")
 print("========================")
 # 2) Revisa si es non
 print("Escribe el número entero a revisar por non o par:")
-inp = "COLOCA AQUÍ TU CÓDIGO"
-res = True
+inp = int(input())
+# Ref: http://book.pythontips.com/en/latest/ternary_operators.html
+# (if_test_is_false, if_test_is_true)[test]
+res = ["non", "par"][((inp % 2) == 0)]
 print("El número", inp, "es", res)
