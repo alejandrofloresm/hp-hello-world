36a672fab9cfc6ac629643faaade967df4eee213
Alejandro Flores <alejandro@esporaestudio.com>
2019-08-01 19:13:17 -0500

    Se agrega la estructura para la actividad 2

---
+++ act2.py
@@ -0,0 +1,16 @@
+# Ejercicio 2
+# 1) Revisa si son palíndomos
+print("Ejercicio 1: Palíndomos")
+print("========================")
+print("¿Qué palabra quieres revisar por palíndromos?")
+inp = "COLOCA AQUÍ TU CÓDIGO"
+res = True
+print("La palabra -", inp, "- es un palíndomo:", res)
+
+print("\n\n\nEjercicio 2: Non o par")
+print("========================")
+# 2) Revisa si es non
+print("Escribe el número entero a revisar por non o par:")
+inp = "COLOCA AQUÍ TU CÓDIGO"
+res = True
+print("El número", inp, "es", res)
